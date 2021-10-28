b130f828190a52cd57aacef2bf7134f21e21ec47
翠花 <admin@nange.cn>
2021-10-29 02:52:04 +0800

    Update shadowsocks-libev.sh

--- shadowsocks-libev.sh
+++ shadowsocks-libev.sh
@@ -17,6 +17,7 @@ FOLDER="/etc/shadowsocks-libev"
 FILE="/usr/local/bin/ss-server"
 CONF="/etc/shadowsocks-libev/config.json"
 Now_ver_File="/etc/shadowsocks-libev/ver.txt"
+
 libsodium_file="libsodium-1.0.18"
 libsodium_url="https://github.com/jedisct1/libsodium/releases/download/1.0.18-RELEASE/libsodium-1.0.18.tar.gz"
 
